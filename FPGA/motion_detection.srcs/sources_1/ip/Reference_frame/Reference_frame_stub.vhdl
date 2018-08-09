b7257573e0eccba16892c2c6ca899d705f2418e7
Matan Livne <matliv@walla.co.il>
2018-08-09 19:25:38 +0300

    Minor changes

---
+++ FPGA/motion_detection.srcs/sources_1/ip/Reference_frame/Reference_frame_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.1 (win64) Build 1846317 Fri Apr 14 18:55:03 MDT 2017
+-- Date        : Thu Aug  9 19:00:49 2018
+-- Host        : DESKTOP-J13MJ0A running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/programming/GitHub/MotionDetection/FPGA/motion_detection.srcs/sources_1/ip/Reference_frame/Reference_frame_stub.vhdl
+-- Design      : Reference_frame
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Reference_frame is
+  Port ( 
+    clka : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+
+end Reference_frame;
+
+architecture stub of Reference_frame is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,addra[3:0],douta[7:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_3_6,Vivado 2017.1";
+begin
+end;
